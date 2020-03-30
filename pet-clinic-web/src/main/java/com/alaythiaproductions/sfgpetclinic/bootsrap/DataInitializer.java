6786c0b5d8fcc28b38b93a4c3ba0df473c1dd96c
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-03-30 08:56:26 -0500

    Refactored Map Impl to abstract ID generation. Close #23

--- pet-clinic-web/src/main/java/com/alaythiaproductions/sfgpetclinic/bootsrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/alaythiaproductions/sfgpetclinic/bootsrap/DataInitializer.java
@@ -22,14 +22,12 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Crichton");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Stephen");
         owner2.setLastName("King");
 
@@ -38,14 +36,12 @@ public class DataInitializer implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("John");
         vet1.setLastName("Malarky");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Marky");
         vet2.setLastName("Mark");
 
