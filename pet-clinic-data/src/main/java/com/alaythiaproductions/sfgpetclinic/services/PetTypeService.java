10236527fffd568124732f9c43d085f02dc50fd6
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-02 09:07:57 -0500

    Added PetTypeService. Close #29

---
+++ pet-clinic-data/src/main/java/com/alaythiaproductions/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.alaythiaproductions.sfgpetclinic.services;
+
+import com.alaythiaproductions.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
