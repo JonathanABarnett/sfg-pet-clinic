57fed5b2fe24af842848d8f9361874361caa4da4
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-07 11:48:54 -0500

    Implemented Map Impl for Visit Service. Close #46

---
+++ pet-clinic-data/src/main/java/com/alaythiaproductions/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.alaythiaproductions.sfgpetclinic.services;
+
+import com.alaythiaproductions.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
